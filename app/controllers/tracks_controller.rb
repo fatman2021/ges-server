d81dea6b14e1aac2a6de890923abdbc5c5a5367a
Zack Brown <zbrown@base2.io>
2013-03-19 15:43:22 -0400

    added a route to get a track from a collection

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,6 +1,6 @@
 class TracksController < ApplicationController
   respond_to :json
   def show
-  	respond_with Collection.find_by_name(params[:id]).tracks.find(:trackid)
+  	respond_with Collection.find_by_name(params[:id]).tracks.find(params[:trackid])
   end
 end
